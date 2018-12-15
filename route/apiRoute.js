a4024309022b3a519d3feddd29c8de7a213585d1
AshSeitz <ash.seitz94@gmail.com>
2018-12-15 13:35:49 -0500

    git commit -m "api post"

--- route/apiRoute.js
+++ route/apiRoute.js
@@ -1,3 +1,19 @@
 var path = require("path");
 
-module.exports = function(api) {};
+module.exports = function(app) {
+    app.post("/api/", function(req, res) {
+        // req.body hosts is equal to the JSON post sent from the user
+        // This works because of our body parsing middleware
+        var newcustomer = req.body;
+      
+        // Using a RegEx Pattern to remove spaces from newCharacter
+        // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+        newcustomer.routeName = newcustomer.name.replace(/\s+/g, "").toLowerCase();
+      
+        console.log(newcustomer);
+      
+        characters.push(newcustomer);
+      
+        res.json(newcustomer);
+      });
+};
