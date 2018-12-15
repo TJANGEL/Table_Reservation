c64a89bef73433d73c888312bc457e619b6d8769
AshSeitz <ash.seitz94@gmail.com>
2018-12-15 12:59:13 -0500

    git commit -m "nodemon"

--- server.js
+++ server.js
@@ -2,8 +2,7 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
-var app = require("./route/htmlRoute.js")
-var api = require("./route/apiRoute.js")
+
 
 // Sets up the Express App
 // =============================================================
@@ -13,6 +12,10 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+require("./route/htmlRoute.js")(app);
+require("./route/apiRoute.js")
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
