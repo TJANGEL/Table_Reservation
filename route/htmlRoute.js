53cd55c9054285c2981e626c0b30b76d3b765299
Dante <42275387+TJANGEL@users.noreply.github.com>
2018-12-15 11:50:09 -0500

    updated paths in html

--- route/htmlRoute.js
+++ route/htmlRoute.js
@@ -1,15 +1,15 @@
-var path = require("path")
+var path = require("path");
 
-module.exports = function(app){
-//home page
-app.get("/", function(req, res) {
+module.exports = function(app) {
+  //home page
+  app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
-//reservation form page
+  //reservation form page
   app.get("/reserve", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/resform.html"));
   });
-//table view page
+  //table view page
   app.get("/tables", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/resview.html"));
   });
