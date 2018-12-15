38dad349a22721c507ef2966bff9f8b4c4027b5a
AshSeitz <ash.seitz94@gmail.com>
2018-12-15 11:45:46 -0500

    git commit -m "require route"

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+var app = require("./route/htmlRoute.js")
 
 // Sets up the Express App
 // =============================================================
